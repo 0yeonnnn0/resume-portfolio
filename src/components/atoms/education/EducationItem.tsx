0a276686bf1922aef4bab8afb9258a17ea7b3f2b
lemonsoda1 <angrybird2600@gmail.com>
2025-01-06 18:54:08 -0800

    [feat] add HoverHighlight - Education

--- src/components/atoms/education/EducationItem.tsx
+++ src/components/atoms/education/EducationItem.tsx
@@ -1,3 +1,5 @@
+import { HoverHighlight } from "../HoverHighlight";
+
 interface EducationItemProps {
   institution: string;
   degree: string;
@@ -13,11 +15,15 @@ export default function EducationItem({
 }: EducationItemProps) {
   return (
     <div className="mb-6">
-      <h3 className="text-lg font-semibold">{institution}</h3>
-      <p className="text-sm text-gray-600">{degree}</p>
-      <p className="text-sm text-gray-600">
-        {location} | {duration}
-      </p>
+      <HoverHighlight className="text-lg font-semibold">
+        {institution}
+      </HoverHighlight>
+      <div className="ml-2">
+        <p className="text-sm text-gray-600">{degree}</p>
+        <p className="text-sm text-gray-600">
+          {location} | {duration}
+        </p>
+      </div>
     </div>
   );
 }
